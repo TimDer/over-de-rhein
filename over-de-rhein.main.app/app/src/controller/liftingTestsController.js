d505203699fdb043c20ad98c2c2e38f6fcb3553d
TimDer <timderksen97@icloud.com>
2020-11-04 14:08:21 +0100

    lifting tests add values

--- over-de-rhein.main.app/app/src/controller/liftingTestsController.js
+++ over-de-rhein.main.app/app/src/controller/liftingTestsController.js
@@ -3,6 +3,21 @@ exports.liftingTestView = (req, res) => {
         res.redirect("/");
     }
     else {
-        res.render("liftingTests", { headTitle: "Over de rhein", topTitle: "Hijstabel: 47" });
+        res.render("liftingTests", {
+            headTitle: "Over de rhein", topTitle: "Hijstabel: 47",
+            
+            dateDrawn: "2019-11-25",
+            mainBoomLength: 45,
+            mechSectionBoomLength: 32,
+
+            jibBoomLength: 84,
+            hoistingCablePartsAmount: 463,
+            swingAngle: 11,
+            ownMassBallast: 31,
+            permissibleOperatingLoad: 55,
+            lbmInEffect: 47,
+            testLoad: 03,
+            Agreed: "",
+        });
     }
 }
