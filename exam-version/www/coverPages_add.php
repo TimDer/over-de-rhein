39f60855485f3d3b9746d7c7a823d2ea62602028
TimDer <timderksen97@icloud.com>
2020-11-10 17:20:14 +0100

    Add name attribute

--- exam-version/www/coverPages_add.php
+++ exam-version/www/coverPages_add.php
@@ -36,49 +36,49 @@ if (isset($_SESSION["user_id"])) {
     <a href="/" class="btn btn-primary coverPages">Terug naar overzigt</a>
 
     <div class="content-container">
-        <form action="#" method="POST">
+        <form action="coverPages_add.submit.php" method="POST">
             <div class="row">
                 <div class="col-lg">
-                    <p><label for="#">TCVT Nummer</label> <input type="number" min="0" class="form-control" value="<?php echo $TCVTNumber; ?>"></p>
-                    <p><label for="#">Keuringsdatum</label> <input type="text" class="form-control" value="<?php echo $inspectionDate; ?>"></p>
-                    <p><label for="#">Uitvoerder</label> <input type="text" class="form-control" value="<?php echo $executor; ?>"></p>
-                    <p><label for="#">Deskundige</label> <input type="text" class="form-control" value="<?php echo $specialist; ?>"></p>
-                    <p><label for="#">Opstelling kraan</label> <input type="text" class="form-control" value="<?php echo $crainSetup; ?>"></p>
-                    <p><label for="#">Uitvoering toren haakhoogte</label> <input type="text" class="form-control" value="<?php echo $executionTowerHookHeight; ?>"></p>
-                    <p><label for="#">Soort giek</label> <input type="text" class="form-control" value="<?php echo $boomType; ?>"></p>
-                    <p><label for="#">Teleskoopgiek delen</label> <input type="number" class="form-control" value="<?php echo $telescopicBoomParts; ?>"></p>
-                    <p><label for="#">Opbouwgiek meters</label> <input type="number" class="form-control" value="<?php echo $constructionBoomMeters; ?>"></p>
-                    <p><label for="#">hulpgiek meters</label> <input type="number" class="form-control" value="<?php echo $jibBoomMeters; ?>"></p>
-                    <p><label for="#">Fly jib delen</label> <input type="number" class="form-control" value="<?php echo $flyJibParts; ?>"></p>
-                    <p><label for="#">Gieklengte</label> <input type="number" class="form-control" value="<?php echo $BoomLength; ?>"></p>
+                    <p><label for="#">TCVT Nummer</label> <input type="number" min="0" class="form-control" name="TCVTNumber" value="<?php echo $TCVTNumber; ?>"></p>
+                    <p><label for="#">Keuringsdatum</label> <input type="date" class="form-control" name="inspectionDate" value="<?php echo $inspectionDate; ?>"></p>
+                    <p><label for="#">Uitvoerder</label> <input type="text" class="form-control" name="executor" value="<?php echo $executor; ?>"></p>
+                    <p><label for="#">Deskundige</label> <input type="text" class="form-control" name="specialist" value="<?php echo $specialist; ?>"></p>
+                    <p><label for="#">Opstelling kraan</label> <input type="text" class="form-control" name="crainSetup" value="<?php echo $crainSetup; ?>"></p>
+                    <p><label for="#">Uitvoering toren haakhoogte</label> <input type="text" name="executionTowerHookHeight" class="form-control" value="<?php echo $executionTowerHookHeight; ?>"></p>
+                    <p><label for="#">Soort giek</label> <input type="text" class="form-control" name="boomType" value="<?php echo $boomType; ?>"></p>
+                    <p><label for="#">Teleskoopgiek delen</label> <input type="number" class="form-control" name="telescopicBoomParts" value="<?php echo $telescopicBoomParts; ?>"></p>
+                    <p><label for="#">Opbouwgiek meters</label> <input type="number" class="form-control" name="constructionBoomMeters" value="<?php echo $constructionBoomMeters; ?>"></p>
+                    <p><label for="#">hulpgiek meters</label> <input type="number" class="form-control" name="jibBoomMeters" value="<?php echo $jibBoomMeters; ?>"></p>
+                    <p><label for="#">Fly jib delen</label> <input type="number" class="form-control" name="flyJibParts" value="<?php echo $flyJibParts; ?>"></p>
+                    <p><label for="#">Gieklengte</label> <input type="number" class="form-control" name="BoomLength" value="<?php echo $BoomLength; ?>"></p>
                 </div>
                 <div class="col-lg">
-                    <p><label for="#">Topbaar</label> <input type="checkbox" class="form-control" <?php echo $topable; ?>></p>
-                    <p><label for="#">Loopkat</label> <input type="checkbox" class="form-control" <?php echo $trolley; ?>></p>
-                    <p><label for="#">Verstelbare giek</label> <input type="checkbox" class="form-control" <?php echo $adjustableBoom; ?>></p>
-                    <p><label for="#">Soort stempels</label> <input type="checkbox" class="form-control" <?php echo $stampsType; ?>></p>
+                    <p><label for="#">Topbaar</label> <input type="checkbox" class="form-control" name="topable" <?php echo $topable; ?>></p>
+                    <p><label for="#">Loopkat</label> <input type="checkbox" class="form-control" name="trolley" <?php echo $trolley; ?>></p>
+                    <p><label for="#">Verstelbare giek</label> <input type="checkbox" class="form-control" name="adjustableBoom" <?php echo $adjustableBoom; ?>></p>
+                    <p><label for="#">Soort stempels</label> <input type="checkbox" class="form-control" name="stampsType" <?php echo $stampsType; ?>></p>
                     <p>
                         <label for="#">Tekortkomingen</label>
                         <div class="row">
                             <div class="col">
-                                <p>Ja: <input type="radio" name="shortcomings" class="form-control" <?php echo $shortcomings_yes; ?>></p>
+                                <p>Ja: <input type="radio" name="shortcomings" class="form-control" value="1" <?php echo $shortcomings_yes; ?>></p>
                             </div>
                             <div class="col">
-                                <p>Nee: <input type="radio" name="shortcomings" class="form-control" <?php echo $shortcomings_no; ?>></p>
+                                <p>Nee: <input type="radio" name="shortcomings" class="form-control" value="0" <?php echo $shortcomings_no; ?>></p>
                             </div>
                         </div>
                     </p>
-                    <p><label for="#">Afgemeeld voor</label> <input type="date" class="form-control" value="<?php echo $signOutBefore; ?>"></p>
+                    <p><label for="#">Afgemeeld voor</label> <input type="date" class="form-control" name="signOutBefore" value="<?php echo $signOutBefore; ?>"></p>
                     <p>
                         <label for="#">Toelichting</label>
-                        <textarea name="" rows="10" class="form-control"><?php echo $elucidation; ?></textarea>
+                        <textarea name="" rows="10" name="elucidation" class="form-control"><?php echo $elucidation; ?></textarea>
                     </p>
-                    <p><label for="#">Werkinstructie</label> <input type="text" class="form-control" value="<?php echo $workInstruction; ?>"></p>
-                    <p><label for="#">Kabelleverancier</label> <input type="text" class="form-control" value="<?php echo $cableSupplier; ?>"></p>
-                    <p><label for="#">Waarmemingen</label> <input type="text" class="form-control" value="<?php echo $observations; ?>"></p>
+                    <p><label for="#">Werkinstructie</label> <input type="text" class="form-control" name="workInstruction" value="<?php echo $workInstruction; ?>"></p>
+                    <p><label for="#">Kabelleverancier</label> <input type="text" class="form-control" name="cableSupplier" value="<?php echo $cableSupplier; ?>"></p>
+                    <p><label for="#">Waarmemingen</label> <input type="text" class="form-control" name="observations" value="<?php echo $observations; ?>"></p>
                     <p></p>
-                    <p><label for="#">Aantal bedrijfsuren</label> <input type="number" min="0" class="form-control" value="<?php echo $operatingHours; ?>"></p>
-                    <p><label for="#">Aflef redenen</label> <input type="text" class="form-control" value="<?php echo $discardReason; ?>"></p>
+                    <p><label for="#">Aantal bedrijfsuren</label> <input type="number" min="0" class="form-control" name="operatingHours" value="<?php echo $operatingHours; ?>"></p>
+                    <p><label for="#">Aflef redenen</label> <input type="text" class="form-control" name="discardReason" value="<?php echo $discardReason; ?>"></p>
                 </div>
             </div>
             <input type="submit" value="Opslaan" class="btn btn-success btn-block">
