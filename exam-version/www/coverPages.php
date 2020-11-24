ced9a08c8af997b4e3639425e73b04e806df0d97
TimDer <timderksen97@icloud.com>
2020-11-24 20:53:07 +0100

    Select all coverPages form the database

--- exam-version/www/coverPages.php
+++ exam-version/www/coverPages.php
@@ -10,68 +10,43 @@ if (isset($_SESSION["user_id"])) {
     <a href="/coverPages_add.php?type=cable" class="btn btn-primary coverPages">Een Kabel-check-lijst toevoegen</a>
     
     <div class="content-container">
-        <div class="row">
-            <div class="col">
-                <h1>Hijs-testen</h1>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Actie</th>
-                            <th>Datum</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        /*$query = "SELECT coverPages.coverPagesID, coverPages.inspectionDate, coverPageStatus.statusType FROM `coverPages` INNER JOIN coverPageStatus ON coverPages.coverPageStatusID=coverPageStatus.coverPageStatusID";
-                        $result = mysqli_query($conn, $query);
+        <h1>Overzicht</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Actie</th>
+                    <th>Datum</th>
+                    <th>Type</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $query = "SELECT `coverPages`.`coverPagesID`,
+                                `coverPages`.`inspectionDate`,
+                                `coverPageStatus`.`statusType`,
+                                `cableChecklists`.`cableChecklistsID` FROM `coverPages`
+                        LEFT JOIN `cableChecklists` ON `cableChecklists`.`coverPagesID` = `coverPages`.`coverPagesID`
+                        INNER JOIN `coverPageStatus` ON `coverPages`.`coverPageStatusID` = `coverPageStatus`.`coverPageStatusID`";
+                $result = mysqli_query($conn, $query);
 
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                ?>
-                                <tr>
-                                    <td><a href="/coverPages_add.php?edit=<?php echo $row["coverPagesID"]; ?>"><?php echo $row["coverPagesID"]; ?></a></td>
-                                    <td><a href="/liftingTests.php?edit=<?php echo $row["coverPagesID"]; ?>" class="btn btn-primary">Bewerken</a></td>
-                                    <td><a href="/cableChecklists.php?edit=<?php echo $row["coverPagesID"]; ?>" class="btn btn-primary">Bewerken</a></td>
-                                    <td><?php echo date("d-m-Y", strtotime($row["inspectionDate"])); ?></td>
-                                    <td><?php echo $row["statusType"]; ?></td>
-                                </tr>
-                                <?php
-                            }
-                        }
-                        else {*/
-                            ?>
-                            <tr>
-                                <td><a href="/coverPages_add.php?edit=47&type=test" class="btn btn-primary">Bewerk: 47</a></td>
-                                <td>21-02-2020</td>
-                                <td>Open</td>
-                            </tr>
-                            <?php
-                        //}
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $type = ($row["cableChecklistsID"] === NULL) ? "test" : "cable";
+                        $typeDisplay = ($row["cableChecklistsID"] === NULL) ? "Hijs-testen" : "Kabel-check-lijst";
                         ?>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col">
-                <h1>Kabel-check-lijst</h1>
-                <table class="table">
-                    <thead>
                         <tr>
-                            <th>Actie</th>
-                            <th>Datum</th>
-                            <th>Status</th>
+                            <td><a href="/coverPages_add.php?edit=<?php echo $row["coverPagesID"]; ?>&type=<?php echo $type; ?>" class="btn btn-primary">Bewerk: <?php echo $row["coverPagesID"]; ?></a></td>
+                            <td><?php echo date("d-m-Y", strtotime($row["inspectionDate"])); ?></td>
+                            <td><?php echo $typeDisplay; ?></td>
+                            <td><?php echo $row["statusType"]; ?></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><a href="/coverPages_add.php?edit=47&type=cable" class="btn btn-primary">Bewerk: 48</a></td>
-                            <td>22-02-2020</td>
-                            <td>Open</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                        <?php
+                    }
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
 
     <?php require __DIR__ . "/template/footer.php";
