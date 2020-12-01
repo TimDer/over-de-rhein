7c1fc21aa70cd975d6238daf03e981aeac6fb7ae
TimDer <timderksen97@icloud.com>
2020-12-01 21:13:49 +0100

    Display users in table

--- exam-version/www/admin/index.php
+++ exam-version/www/admin/index.php
@@ -3,7 +3,15 @@
 session_start();
 
 if (isset($_SESSION["admin_id"])) {
-    
+    require_once __DIR__ . "/../db.php";
+
+    $result_array = array();
+    $query = mysqli_query($conn, "SELECT `users`.`userID`, `users`.`userName`, `userType`.`type` FROM `users` LEFT JOIN `userType` ON `users`.`userTypeID` = `userType`.`userTypeID`");
+    if ($query->num_rows > 0) {
+        while ($result = $query->fetch_assoc()) {
+            $result_array[] = $result;
+        }
+    }
 
     require __DIR__ . "/../template/header.php"; ?>
 
@@ -15,15 +23,19 @@ if (isset($_SESSION["admin_id"])) {
                 <tr>
                     <th>Gebruiker nummer</th>
                     <th>Gebruikersnaam</th>
+                    <th>Type gebruiker</th>
                     <th>Actie</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>User_1</td>
-                    <td><a href="user_add_edit.php?id=1" class="btn btn-primary">Bewerken</a> <a href="#" class="btn btn-primary">Verwijderen</a></td>
-                </tr>
+                <?php foreach ($result_array AS $key => $value) { ?>
+                    <tr>
+                        <td><?php echo $value["userID"] ?></td>
+                        <td><?php echo $value["userName"] ?></td>
+                        <td><?php echo $value["type"] ?></td>
+                        <td><a href="user_add_edit.php?id=<?php echo $value["userID"] ?>" class="btn btn-primary">Bewerken</a> <a href="#" class="btn btn-primary">Verwijderen</a></td>
+                    </tr>
+                <?php } ?>
             </tbody>
         </table>
     </div>
